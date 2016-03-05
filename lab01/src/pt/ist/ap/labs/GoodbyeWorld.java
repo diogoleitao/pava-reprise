ca4959c71803acb0775e6bef00ef277ffb640953
Diogo Leitão <diogo.n.leitao@gmail.com>
2016-03-05 18:32:40 +0000

    Added 1st lab files

---
+++ lab01/src/pt/ist/ap/labs/GoodbyeWorld.java
@@ -0,0 +1,13 @@
+package pt.ist.ap.labs;
+
+public class GoodbyeWorld implements Message{
+
+	@Override
+	public void say() {
+		// TODO Auto-generated method stub
+		System.out.println("Goodbye World!");
+		
+	}
+	
+
+}
