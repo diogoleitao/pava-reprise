81c0705a440504b8ba82df24c5b989c08426cf69
Diogo Leitão <diogo.n.leitao@gmail.com>
2016-03-07 00:48:17 +0000

    Some errors of 2nd lab solved; NPE keeps on appearing

--- lab02/src/pt/ist/ap/labs/Program.java
+++ lab02/src/pt/ist/ap/labs/Program.java
@@ -22,7 +22,7 @@ public class Program {
 				try {
 					lastClassUsed = command[1];
 					lastInstanceUsed = Class.forName(lastClassUsed).newInstance();
-					System.out.println("class " + lastClassUsed);
+					System.out.println(lastInstanceUsed.getClass());
 				} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
 					System.out.println(e.getMessage());
 				}
@@ -31,7 +31,7 @@ public class Program {
 					System.out.println("No class defined previously. Invoke Class command first.");
 				else {
 					variables.put(command[1], lastInstanceUsed);
-					System.out.println("Saved name for object of type: class " + lastClassUsed);
+					System.out.println("Saved name for object of type: " + lastInstanceUsed.getClass());
 				}
 			} else if (command[0].equals("Get")) {
 				if (variables.isEmpty())
@@ -39,7 +39,11 @@ public class Program {
 				else {
 					lastInstanceUsed = variables.get(command[1]);
 					if (lastInstanceUsed == null)
-						System.out.println("No object stored with name " + command[1]);
+						System.out.println("No object stored with name '" + command[1] + "'.");
+					else {
+						lastClassUsed = lastInstanceUsed.getClass().toGenericString();
+						System.out.println(lastInstanceUsed.getClass());
+					}
 				}
 			} else if (command[0].equals("Index")) {
 				int index = Integer.parseInt(command[1]);
@@ -51,21 +55,24 @@ public class Program {
 				System.out.println("Trying generic command: " + command[0]);
 				try {
 					if (command.length > 1)
-						findMethod(lastInstanceUsed.getClass(), command[0], ((Object)command[1]).getClass()).invoke(lastInstanceUsed, (Object) command[1]);
+						System.out.println(findMethod(lastInstanceUsed.getClass(), command[0], ((Object) command[1]).getClass()).invoke(lastInstanceUsed, (Object) command[1]));
 					else
-						findMethod(lastInstanceUsed.getClass(), command[0], null).invoke(lastInstanceUsed, (Object) null);
+						System.out.println(findMethod(lastInstanceUsed.getClass(), command[0], null).invoke(lastInstanceUsed));
 				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException e) {
-					System.out.println(e.getMessage());
+					e.printStackTrace();
 				}
 			}
 		}
 	}
-	
+
 	public static Method findMethod(Class<?> type, String name, Class<?> argType) throws NoSuchMethodException {
 		try {
-			return type.getMethod(name, argType);
+			if (argType == null)
+				return type.getMethod(name);
+			else
+				return type.getMethod(name, argType);
 		} catch (NoSuchMethodException e) {
-			if (argType == Object.class) 
+			if (argType == Object.class)
 				throw new NoSuchMethodException(name);
 			else
 				return findMethod(type, name, argType.getSuperclass());
