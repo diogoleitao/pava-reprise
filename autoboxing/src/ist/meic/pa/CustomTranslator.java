2c5b884fc57c07ffaeedee4b23eda57802596db3
Diogo Leitão <diogo.n.leitao@gmail.com>
2016-03-29 16:52:51 +0100

    Final commit for submission (without extensions)

--- autoboxing/src/ist/meic/pa/CustomTranslator.java
+++ autoboxing/src/ist/meic/pa/CustomTranslator.java
@@ -12,18 +12,25 @@ import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 
 /**
- *Custom Translator class used to instrument the classes about to be profiled.
+ * Custom Translator class used to instrument the classes about to be profiled.
  */
-
 public class CustomTranslator implements Translator {
 
 	@Override
 	public void start(ClassPool pool) throws NotFoundException, CannotCompileException {
 	}
-	
+
 	/**
-	 * The onLoad method checks if the function being loaded belongs to this project's
-	 * package, or to Javassist's, and, if not, calls the InstrumentClass method on the class.
+	 * The onLoad method checks if the function being loaded belongs to this
+	 * project's package, or to Javassist's, and, if not, calls the
+	 * instrumentClass method with that class.
+	 *
+	 * @param pool
+	 *            the pool of classes
+	 * @param ctClass
+	 *            the class being loaded
+	 * @throws NotFoundException
+	 * @throws CannotCompileException
 	 */
 
 	@Override
@@ -42,56 +49,55 @@ public class CustomTranslator implements Translator {
 			}
 		}
 	}
-	
+
 	/**
-	 * Method that checks the input class for boxing/unboxing operations and instruments
-	 * it if such operations are found.
-	 * If a call to the valueOf() or any of the *Value() methods is found, that call
-	 * is replaced by a block of code containing that same call followed by a call
-	 * to the updateAutoboxingCounter method, if the types are compatible.
-	 * The compatibility is checked with the primitiveType method.
-	 * This method also looks for the main() method of the instrumented class and
-	 * adds the printOutput() method to it.
+	 * Method that checks the input class for autoboxing operations and
+	 * instruments it if such operations are found. If a call to the valueOf()
+	 * or any of the *Value() methods is found, that call is replaced by a block
+	 * of code containing that same call followed by a call to the
+	 * updateAutoboxingCounter() method, if the types are compatible. The
+	 * compatibility is checked with the checkTypeCompatibility() method. This
+	 * method also looks for the main() method of the instrumented class and
+	 * adds the printOutput() method to it, so that when the input program is
+	 * finished, the information gathered by the profiler is printed.
 	 * 
-	 * @param ctClass Class to be instrumented
+	 * @param ctClass
+	 *            Class to be instrumented
 	 * @throws CannotCompileException
 	 */
-
-	private void instrumentClass(final CtClass ctClass) throws CannotCompileException {
-		for (final CtMethod method : ctClass.getDeclaredMethods()) {
+	private void instrumentClass(CtClass ctClass) throws CannotCompileException {
+		for (CtMethod method : ctClass.getDeclaredMethods()) {
 			final String methodName = method.getLongName();
 			method.instrument(new ExprEditor() {
 				@Override
 				public void edit(MethodCall methodCall) throws CannotCompileException {
 					try {
 						CtClass[] parameterTypes = methodCall.getMethod().getParameterTypes();
-						
 						String parameterClassName = "";
 						String codeTemplate =
-							"{" +
-									"$_ = $proceed($$);" +
-									"if (ist.meic.pa.Storage.primitiveType($_, $0)) {" +
-										"ist.meic.pa.Storage.updateAutoboxingCounter(\"%s\");" +
-									"}" +
-							"}";
+								"{" +
+										"$_ = $proceed($$);" +
+										"if (ist.meic.pa.Storage.checkTypeCompatibility($_, $0)) {" +
+											"ist.meic.pa.Storage.updateAutoboxingCounter(\"%s\");" +
+										"}" +
+								"}";
 						String autoboxingMethodName = methodCall.getMethod().getLongName();
-						
 						String incompleteKey = methodName + Storage.SEPARATOR;
 						String boxed = " boxed ";
 						String unboxed = " unboxed ";
-						
+
 						if (autoboxingMethodName.contains("valueOf")) {
 							parameterClassName = getWrapperType(parameterTypes[0].getName());
-							incompleteKey += parameterClassName + Storage.SEPARATOR;
-							methodCall.replace(String.format(codeTemplate, incompleteKey + boxed, incompleteKey + boxed));
+							String finalKey = incompleteKey + parameterClassName + Storage.SEPARATOR + boxed;
+							methodCall.replace(String.format(codeTemplate, finalKey, finalKey));
 
 						} else if (autoboxingMethodName.contains("Value")) {
 							String methodCallName = methodCall.getMethodName();
-							parameterClassName = getWrapperType(methodCallName.substring(0, methodCallName.indexOf("Value")));
-							incompleteKey += parameterClassName + Storage.SEPARATOR;
-							methodCall.replace(String.format(codeTemplate, incompleteKey + unboxed, incompleteKey + unboxed));
+							String primitiveType = methodCallName.substring(0, methodCallName.indexOf("Value"));
+							parameterClassName = getWrapperType(primitiveType);
+							String finalKey = incompleteKey + parameterClassName + Storage.SEPARATOR + unboxed;
+							methodCall.replace(String.format(codeTemplate, finalKey, finalKey));
 						}
-
 					} catch (NotFoundException e) {
 						throw new RuntimeException(e);
 					}
@@ -102,15 +108,15 @@ public class CustomTranslator implements Translator {
 				method.insertAfter("ist.meic.pa.Storage.printOutput();");
 		}
 	}
-	
+
 	/**
-	 * Translates the short name of a type, obtained from a boxing operation, to its
-	 * full, long name.
+	 * Translates the primitive type designation, obtained from an autoboxing
+	 * operation, to its wrapper correspondent.
 	 * 
 	 * @param primitiveType
-	 * @return
+	 *            the primitive type
+	 * @return the wrapper type
 	 */
-
 	private String getWrapperType(String primitiveType) {
 		if (primitiveType.equals("int"))
 			return "java.lang.Integer";
--- autoboxing/src/ist/meic/pa/Storage.java
+++ autoboxing/src/ist/meic/pa/Storage.java
@@ -3,88 +3,94 @@ package ist.meic.pa;
 import java.util.TreeMap;
 
 /**
- * Class containing a TreeMap and the respective methods needed to store the 
- * boxing/unboxing operations.
- * The keys for the TreeMap structure consist of the name of the method that 
- * called the boxing/unboxing operation (obtained with getLongName()), the 
- * the class being boxed/unboxed, and the type of boxing operation, in that
- * order, separated with the 'SEPARATOR' string.
+ * Class containing a TreeMap with the methods needed to store the autoboxing
+ * operations. The keys for the TreeMap structure consist of the name of the
+ * method that forced the autoboxing operation, the class of the parameter being
+ * autoboxed and the type of the autoboxing operation, in that order, separated
+ * with the 'SEPARATOR' token.
  */
-
 public class Storage {
 
 	public static final String SEPARATOR = "separador";
 
 	private static TreeMap<String, Integer> autoboxingCounters = new TreeMap<String, Integer>();
 
-	public static void updateAutoboxingCounter(String completeKey) {
-		if (!autoboxingCounters.containsKey(completeKey))
-			autoboxingCounters.put(completeKey, new Integer(1));
+	/**
+	 * If the string "method + parameter class + box/unbox operation" used as
+	 * the TreeMap key is not present, adds an entry with its counter set to 1.
+	 * Otherwise, it just increments the respective counter
+	 *
+	 * @param methodKey
+	 *            the key, built as explained above
+	 */
+	public static void updateAutoboxingCounter(String methodKey) {
+		if (!autoboxingCounters.containsKey(methodKey))
+			autoboxingCounters.put(methodKey, new Integer(1));
 		else {
-			int currentCounter = autoboxingCounters.get(completeKey);
-			autoboxingCounters.put(completeKey, new Integer(++currentCounter));
+			int currentCounter = autoboxingCounters.get(methodKey);
+			autoboxingCounters.put(methodKey, new Integer(++currentCounter));
 		}
 	}
-	
-	/**
-	 * Method used to print the output of the profiler program.
-	 * The output is printed in the same order as the keys are stored in the TreeMap structure,
-	 * which is the expected order of the output.
-	 */
 
+	/**
+	 * Prints the output of the profiler. The output is printed in the same
+	 * order as the keys are stored in the TreeMap, which is the expected order
+	 * of the output.
+	 */
 	public static void printOutput() {
 		for (String methodName : autoboxingCounters.keySet()) {
 			String[] split = methodName.split(SEPARATOR);
 			String method = split[0];
 			String parameterClass = split[1];
-			String boxing = split[2];
-			System.err.println(method + boxing + autoboxingCounters.get(methodName) + " " + parameterClass);
+			String operation = split[2];
+			System.err.println(method + operation + autoboxingCounters.get(methodName) + " " + parameterClass);
 		}
-
 	}
-	
+
 	/**
-	 * Methods used to assert the boxing compatibility of the types involved,
-	 * so as to prevent false positives when the types are not compatible.
+	 * Methods used to assert the autoboxing compatibility of the types
+	 * involved, so as to prevent false positives when the types are not
+	 * compatible (autoboxing should only be accounted for between a primitive
+	 * type and its wrapper equivalent).
 	 */
 
-	private static boolean primitiveType(String type, String clazz) {
+	private static boolean checkTypeCompatibility(String type, String clazz) {
 		return clazz.equalsIgnoreCase(type);
 	}
 
-	public static boolean primitiveType(int i, Object o) {
-		return primitiveType("integer", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(int i, Object o) {
+		return checkTypeCompatibility("integer", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(double d, Object o) {
-		return primitiveType("double", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(double d, Object o) {
+		return checkTypeCompatibility("double", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(short s, Object o) {
-		return primitiveType("short", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(short s, Object o) {
+		return checkTypeCompatibility("short", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(float f, Object o) {
-		return primitiveType("float", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(float f, Object o) {
+		return checkTypeCompatibility("float", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(long l, Object o) {
-		return primitiveType("long", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(long l, Object o) {
+		return checkTypeCompatibility("long", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(byte b, Object o) {
-		return primitiveType("byte", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(byte b, Object o) {
+		return checkTypeCompatibility("byte", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(char c, Object o) {
-		return primitiveType("character", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(char c, Object o) {
+		return checkTypeCompatibility("character", o.getClass().getSimpleName());
 	}
 
-	public static boolean primitiveType(boolean b, Object o) {
-		return primitiveType("boolean", o.getClass().getSimpleName());
+	public static boolean checkTypeCompatibility(boolean b, Object o) {
+		return checkTypeCompatibility("boolean", o.getClass().getSimpleName());
 	}
-	
-	public static boolean primitiveType(Object o1, Object o2) {
+
+	public static boolean checkTypeCompatibility(Object o1, Object o2) {
 		return true;
 	}
 }
